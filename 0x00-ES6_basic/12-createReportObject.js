221d9d71b8e53be2a084428ce5a98f7803334d70
Samson Wayne Muhadia <smuhadia4@gmail.com>
2024-04-22 22:59:48 +0300

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments() {
-      return Object.keys(this.allEmployees).length;
-    }
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
