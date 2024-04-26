8d2e73c7a2a232d4559d6ed89c3ee9cb94ce0918
Samson Wayne Muhadia <smuhadia4@gmail.com>
2024-04-26 12:37:39 +0300

    adjusted 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,13 @@
+/**
+ * Represents a class room.
+ */
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
